bffc44b9e0121c1e0612bb54b356f013d9e841fb
madworld <madison.bryan@gmail.com>
2012-09-28 10:32:55 -0400

    Added more POIs

--- data.js
+++ data.js
@@ -2,6 +2,13 @@ var atlanta = {
     'poi': [
         {name: 'Piedmont Park', 'lat': 33.786567, 'long': -84.373970},
         {name: 'WebMD', 'lat': 33.787779, 'long': -84.382972},
-        {name: 'GA State Capital', 'lat': 33.749160, 'long': -84.388142}
+        {name: 'GA State Capital', 'lat': 33.749160, 'long': -84.388142},
+        {name: 'Georgia Aquarium', 'lat': 33.762387, 'long': -84.394576},
+        {name: 'Zoo Atlanta', 'lat': 33.732599, 'long': -84.371277},
+        {name: 'Fox Theater', 'lat': 33.772639, 'long': -84.38495},
+        {name: 'Georgia Dome', 'lat': 33.757336, 'long': -84.401179},
+        {name: 'Turner Field', 'lat': 33.735544, 'long': -84.389567},
+        {name: 'Margaret Mitchell House', 'lat': 33.781410, 'long': -84.384588},
+        {name: 'Jimmy Carter Library', 'lat': 33.767036, 'long': -84.357056}
     ]
 };
